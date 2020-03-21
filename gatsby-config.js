ff43388e9caffeac4c3b99bf0a1404f002c9002f
Alex Mains <mainsaj@gmail.com>
2020-03-20 20:58:41 -0500

    social-links.tsx

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require(`dotenv`).config({
 module.exports = {
   siteMetadata: {
     siteTitle: "Nic Mains",
-    siteTitleAlt: `Emma - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Nic Mains Portfolio`,
   },
   plugins: [
     {
